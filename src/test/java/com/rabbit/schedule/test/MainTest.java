3154c463c182accfaa11c8e54c9235c70b38476a
guoyankuiBASH <yankuiguo@gmail.com>
2016-11-11 17:40:46 +0800

    add properties file

---
+++ src/test/java/com/rabbit/schedule/test/MainTest.java
@@ -0,0 +1,15 @@
+package com.rabbit.schedule.test;
+
+import org.junit.Test;
+
+import cn.uncode.schedule.util.ScheduleUtil;
+
+public class MainTest {
+	
+	@Test
+	public void testProperties(){
+		System.out.println("qqqqqqqqq");
+		System.out.println(ScheduleUtil.getServerCode());
+	}
+
+}
