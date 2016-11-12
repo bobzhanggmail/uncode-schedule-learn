22a2ee7ea1c76c141159aff75425c6dfca540283
guoyankui <yankuiguo@gmail.com>
2016-11-12 20:09:21 +0800

    add black ip test

--- src/test/java/com/rabbit/schedule/test/MainTest.java
+++ src/test/java/com/rabbit/schedule/test/MainTest.java
@@ -26,5 +26,27 @@ public class MainTest {
 		System.out.println("qqqqqqqqq");
 		System.out.println(ScheduleUtil.getServerCode());
 	}
+	
+	@Test
+	public void testIndex(){
+		List<String> list = new ArrayList<String>();
+		list.add("100001");
+		list.add("000021");
+		list.add("000031");
+		list.add("000012");
+		
+		System.out.println(list.indexOf("000012"));
+	}
+	
+	@Test
+	public void testSub(){
+		List<String> serverList = new ArrayList<String>();
+		serverList.add("192.168.7.231$urerjefru$291929");
+		List<String> serverIpList = new ArrayList<String>(serverList.size());
+		for(String ser:serverList){
+			serverIpList.add(ser.substring(0, ser.indexOf("$")));
+		}
+		System.out.println(serverIpList.get(0));
+	}
 
 }
