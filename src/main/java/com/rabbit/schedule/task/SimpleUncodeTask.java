1b9606e5ee16821545cf7b1f8f9483e0fc1b030d
guoyankui <yankuiguo@gmail.com>
2016-11-13 22:04:07 +0800

    add uncode task example

---
+++ src/main/java/com/rabbit/schedule/task/SimpleUncodeTask.java
@@ -0,0 +1,30 @@
+package com.rabbit.schedule.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleUncodeTask {
+
+	private static int i = 0;
+    private Logger log = LoggerFactory.getLogger(SimpleUncodeTask.class);
+    
+    public void uncodePrint1(){
+    	log.info("===========uncodePrint1 start!=========");
+        log.info("uncodePrint1:"+i);i++;
+        log.info("===========uncodePrint1 end !=========");
+    }
+    
+    public void uncodePrint2(){
+    	log.info("===========uncodePrint2 start!=========");
+        log.info("uncodePrint2:"+i);i++;
+        log.info("===========uncodePrint2 end !=========");
+    }
+    
+    public void uncodePrint3(){
+    	log.info("===========uncodePrint3 start!=========");
+        log.info("uncodePrint3:"+i);i++;
+        log.info("===========uncodePrint3 end !=========");
+    }
+}
