fd49fcded80fff902614cede4f0d6bfba41f247b
guoyankuiBASH <yankuiguo@gmail.com>
2016-11-25 15:57:48 +0800

    添加手动处理任务的测试

--- src/main/java/com/rabbit/schedule/controller/HelloController.java
+++ src/main/java/com/rabbit/schedule/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.uncode.schedule.ConsoleManager;
+import cn.uncode.schedule.core.TaskDefine;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
@@ -11,6 +14,10 @@ public class HelloController {
 	@RequestMapping("/name")
 	@ResponseBody
 	public String hello(String name){
+		TaskDefine task = new TaskDefine();
+		task.setTargetBean("simpleUncodeTask");
+		task.setTargetMethod("uncodePrint3");
+		ConsoleManager.runTask(task);
 		return "hello " + name;
 	}
 }
